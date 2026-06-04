61fcba829e503952017aab8cc79f3f93327a786d
Penny Chase <mpchase136@gmail.com>
2026-06-04 11:07:00 -0400

    Fixed formatting in V1 and V2

--- esintro/es_calc_v1.exs
+++ esintro/es_calc_v1.exs
@@ -1,19 +1,19 @@
 defmodule EventSourceCalculator.V1 do
 
-def handle_command(%{value: val}, %{cmd: :add, value: v}) do
-	%{value: val + v}
-end
+	def handle_command(%{value: val}, %{cmd: :add, value: v}) do
+		%{value: val + v}
+	end
 
-def handle_command(%{value: val}, %{cmd: :sub, value: v}) do
-	%{value: val - v}
-end
+	def handle_command(%{value: val}, %{cmd: :sub, value: v}) do
+		%{value: val - v}
+	end
 
-def handle_command(%{value: val}, %{cmd: :mul, value: v}) do
-	%{value: val * v}
-end
+	def handle_command(%{value: val}, %{cmd: :mul, value: v}) do
+		%{value: val * v}
+	end
 
-def handle_command(%{value: val}, %{cmd: :div, value: v}) do
-	%{value: val / v}
-end
+	def handle_command(%{value: val}, %{cmd: :div, value: v}) do
+		%{value: val / v}
+	end
 
 end
